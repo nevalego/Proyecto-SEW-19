7d37e6d3de5e694d4f0bd1de1624f4703bd2d1cf
Nerea Valdés Egocheaga <nerevalego@gmail.com>
2019-05-22 00:58:06 +0200

    Comienzo con JS y añadiendo al XML sucesos ep 10

---
+++ EcmaScript/dark.js
@@ -0,0 +1,27 @@
+"use script";
+class Dark(){
+    constructor(){
+
+    }
+}
+
+class Temporada(){
+    constructor(){
+        this.t=1;
+    }
+}
+
+class Episodio(){
+    constructor(){
+        this.
+    }
+}
+
+class Personaje(){
+    constructor(){
+        this.nombre="";
+        this.familia="";
+        this.edad=0;
+        this.contexto="";
+    }
+}
