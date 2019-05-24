01f7ec1ce46854c23e908de796b80a88a24e6fd2
Nerea Valdés Egocheaga <nerevalego@gmail.com>
2019-05-24 19:59:32 +0200

    JS devuelve nombre Dark

--- EcmaScript/dark.js
+++ EcmaScript/dark.js
@@ -1,27 +1,32 @@
 "use script";
-class Dark(){
-    constructor(){
 
+class Dark {
+    constructor() {
+        this.apiKey = 'a5b8fe70c172015d51961225ca93b6ac';
+        this.url = 'https://api.themoviedb.org/3/tv/70523?api_key=' + this.apiKey + '&language=es-ES';
+        this.name = '';
+    }
+
+    cargarDatos() {
+        $.ajax({
+            dataType: "json",
+            url: this.url,
+            method: 'GET',
+            success: function (json) {
+
+                this.mostrar(json);
+            },
+            error: function () {
+                alert("No se pudo obtener JSON de The Movie DataBase API")
+            }
+
+        });
+
+    }
+
+    mostrar(json) {
+        $("main").append("<h1>" + json.name + "</h1>");
     }
 }
 
-class Temporada(){
-    constructor(){
-        this.t=1;
-    }
-}
-
-class Episodio(){
-    constructor(){
-        this.
-    }
-}
-
-class Personaje(){
-    constructor(){
-        this.nombre="";
-        this.familia="";
-        this.edad=0;
-        this.contexto="";
-    }
-}
+let dark = new Dark();
